ddeb3aec7b86a551f5f4b372b332197fe5cdee1e
Amodh <n.amodh@agathsya.net>
2024-05-18 17:52:35 +0530

    Push

--- lib/component/bottom_nav_bar/bindings/bottom_nav_bar_bindings.dart
+++ lib/component/bottom_nav_bar/bindings/bottom_nav_bar_bindings.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:task_swift/component/bottom_nav_bar/controller/bottom_nav_bar_controller.dart';
+import 'package:task_swift/presentation/home_screen/controller/home_screen_controller.dart';
 
 class TaskSwiftNavBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => TaskSwiftNavBarController());
+    Get.lazyPut(() => HomeScreenController());
   }
 }
